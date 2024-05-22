3588f93ae7c39aa1b508a5cb56cb31f7a27ec394
Alejo N. Terreno <anterreno12@gmail.com>
2024-05-22 18:40:32 -0300

    Add files via upload

---
+++ 4.1.1 implementación.py
@@ -0,0 +1,9 @@
+nota_1 = float(input("ingrese la nota 1: "))
+nota_2 = float(input("ingrese la nota 2: "))
+nota_3 = float(input("ingrese la nota 3: "))
+nota_4 = float(input("ingrese la nota 4: "))
+nota_5 = float(input("ingrese la nota 5: "))
+
+promedio = (nota_1 + nota_2 + nota_3 + nota_4 + nota_5)/5
+
+print ("su promedio es: ",promedio)
